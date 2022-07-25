64c75e0ff9aa2a01dd175920f99aef1da1431f30
Andronik <write@reusable.software>
2022-07-25 16:23:24 +0200

    add disputes subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,64 @@
-use clap::Parser;
-use plotters::prelude::*;
+use clap::{Parser, Subcommand};
 
 pub mod subscan;
 
-/// cargo run -- --network kusama --para-id 2023 --up-to-block 11324714
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[clap(version)]
-struct Args {
-    /// Name of the network, e.g. "kusama"
-    #[clap(long, default_value = "kusama")]
-    network: String,
+struct Cli {
+    #[clap(subcommand)]
+    commands: Commands,
+}
 
-    /// Parachain ID to be processed
-    #[clap(long)]
-    para_id: u32,
+#[derive(Subcommand)]
+enum Commands {
+    /// Fetches the backing and inclusion events and writes out csv files to `./out/`.
+    ///
+    /// Example:
+    /// ```
+    /// cargo run -- inclusion --network kusama --para-id 2023 --up-to-block 11324714
+    /// ```
+    Inclusion {
+        /// Name of the network, e.g. "kusama"
+        #[clap(long, default_value = "kusama")]
+        network: String,
 
-    /// The block number up to which we should
-    /// be fetching events, e.g. 11324714
-    #[clap(long)]
-    up_to_block: u32,
+        /// Parachain ID to be processed
+        #[clap(long)]
+        para_id: u32,
 
-    /// How many events to fetch
-    #[clap(long, default_value_t = 500)]
-    num_events: usize,
+        /// The block number up to which we should
+        /// be fetching events, e.g. 13524714
+        #[clap(long)]
+        up_to_block: u32,
+
+        /// How many events to fetch
+        #[clap(long, default_value_t = 500)]
+        num_events: usize,
+    },
+    /// Fetches the dispute invalid votes and writes out a csv file to `./out/`.
+    ///
+    /// Example:
+    /// ```
+    /// cargo run -- disputes --network kusama --from-block 11324714 --num-events 200
+    /// ```
+    Disputes {
+        /// Name of the network, e.g. "kusama"
+        #[clap(long, default_value = "kusama")]
+        network: String,
+
+        /// How many events to fetch
+        #[clap(long, default_value_t = 100)]
+        num_events: usize,
+
+        /// The block number up to which we should
+        /// be fetching events, e.g. 13524714
+        #[clap(long)]
+        up_to_block: u32,
+    },
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Event {
+pub struct InclusionEvent {
     pub block_num: u32,
     pub para_id: u32,
     pub included: bool,
@@ -40,86 +72,75 @@ pub struct PlottingPoint {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
-    let network = &args.network;
-    let block = args.up_to_block;
-    let para_id: u32 = args.para_id;
-    let num_events = args.num_events;
-    let events = subscan::fetch(network, block, para_id, num_events).await?;
+    let cli = Cli::parse();
 
-    let mut last_backed = None;
-    let mut last_included = None;
-    let mut backing_times = Vec::new();
-    let mut inclusion_times = Vec::new();
+    match cli.commands {
+        Commands::Inclusion {
+            network,
+            para_id,
+            up_to_block,
+            num_events,
+        } => {
+            let events =
+                subscan::fetch_inclusion_events(&network, up_to_block, para_id, num_events).await?;
 
-    for event in events.into_iter().filter(|e| e.para_id == para_id) {
-        if event.included {
-            let block_num = event.block_num;
-            if let Some(b) = last_backed {
-                let blocks = block_num.saturating_sub(b);
-                inclusion_times.push(PlottingPoint { block_num, blocks });
+            let mut last_backed = None;
+            let mut last_included = None;
+            let mut backing_times = Vec::new();
+            let mut inclusion_times = Vec::new();
+
+            for event in events.into_iter().filter(|e| e.para_id == para_id) {
+                if event.included {
+                    let block_num = event.block_num;
+                    if let Some(b) = last_backed {
+                        let blocks = block_num.saturating_sub(b);
+                        inclusion_times.push(PlottingPoint { block_num, blocks });
+                    }
+                    last_included = Some(block_num);
+                } else {
+                    let block_num = event.block_num;
+                    if let Some(i) = last_included {
+                        let blocks = block_num.saturating_sub(i);
+                        backing_times.push(PlottingPoint { block_num, blocks });
+                    }
+                    last_backed = Some(block_num);
+                }
             }
-            last_included = Some(block_num);
-        } else {
-            let block_num = event.block_num;
-            if let Some(i) = last_included {
-                let blocks = block_num.saturating_sub(i);
-                backing_times.push(PlottingPoint { block_num, blocks });
+
+            std::fs::create_dir_all("out")?;
+
+            for (data, name) in [(backing_times, "backing"), (inclusion_times, "inclusion")] {
+                if data.is_empty() {
+                    eprintln!("No {name} events found for {para_id}");
+                    continue;
+                }
+                let csv_file = format!("out/{up_to_block}-{name}-{para_id}.csv");
+                let mut wrt = csv::Writer::from_path(&csv_file)?;
+                for p in data.iter().copied() {
+                    wrt.serialize(p)?;
+                }
+                wrt.flush()?;
+                eprintln!("Saved the data to {csv_file}");
             }
-            last_backed = Some(block_num);
         }
-    }
+        Commands::Disputes {
+            network,
+            num_events,
+            up_to_block,
+        } => {
+            let events = subscan::fetch_disputes_events(&network, up_to_block, num_events).await?;
+            let initiators = subscan::fetch_dispute_initiators(&network, events).await?;
 
-    std::fs::create_dir_all("out")?;
+            std::fs::create_dir_all("out")?;
 
-    for (data, name) in [(backing_times, "backing"), (inclusion_times, "inclusion")] {
-        if data.is_empty() {
-            eprintln!("No {name} events found for {para_id}");
-            continue;
+            let csv_file = format!("out/disputes-{network}-{up_to_block}.csv");
+            let mut wrt = csv::Writer::from_path(&csv_file)?;
+            for i in initiators.into_iter() {
+                wrt.serialize(i)?;
+            }
+            wrt.flush()?;
+            eprintln!("Saved the data to {csv_file}");
         }
-        let csv_file = format!("out/{block}-{name}-{para_id}.csv");
-        let mut wrt = csv::Writer::from_path(&csv_file)?;
-        for p in data.iter().copied() {
-            wrt.serialize(p)?;
-        }
-        wrt.flush()?;
-        eprintln!("Saved the data to {csv_file}");
-
-        let out_file_name = format!("out/{block}-{name}-{para_id}.png");
-        let root = BitMapBackend::new(&out_file_name, (1024, 768)).into_drawing_area();
-
-        root.fill(&WHITE)?;
-        let x_first = data.first().unwrap().block_num;
-        let x_last = data.last().unwrap().block_num;
-        let y_max = data.iter().max_by_key(|p| p.blocks).unwrap().blocks as f64 + 1.0;
-        eprintln!("Plotting {name} for blocks {x_first}..{x_last}");
-
-        let mut chart = ChartBuilder::on(&root)
-            .margin(10)
-            .caption(
-                format!("{network}: para_id({para_id}) {name} times"),
-                ("sans-serif", 40),
-            )
-            .set_label_area_size(LabelAreaPosition::Left, 60)
-            .set_label_area_size(LabelAreaPosition::Bottom, 40)
-            .build_cartesian_2d(x_first..x_last, 0.0..y_max)?;
-
-        chart
-            .configure_mesh()
-            .disable_x_mesh()
-            .disable_y_mesh()
-            .x_labels(10)
-            .y_labels(5)
-            .y_desc("Blocks")
-            .draw()?;
-
-        chart
-            .draw_series(data.into_iter().map(|p| {
-                Circle::new((p.block_num as _, p.blocks as _), 3, RED.mix(0.2).filled())
-            }))?;
-
-        root.present().unwrap();
-        eprintln!("The {name} plot has been saved to {out_file_name}");
     }
 
     Ok(())
--- src/subscan.rs
+++ src/subscan.rs
@@ -1,7 +1,11 @@
-use crate::Event;
+use crate::InclusionEvent;
+use anyhow::Context;
 use indicatif::ProgressBar;
 use std::str::FromStr as _;
 use tokio::time::{sleep, Duration};
+
+type SessionIndex = u32;
+type ValidatorIndex = u32;
 
 pub mod events {
     use serde::{Deserialize, Serialize};
@@ -12,39 +16,124 @@ pub mod events {
         pub page: u32,
         pub module: &'static str,
         #[serde(skip_serializing_if = "Option::is_none")]
+        pub call: Option<&'static str>,
+        #[serde(skip_serializing_if = "Option::is_none")]
         pub block_num: Option<u32>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub block_range: Option<String>,
     }
 
-    #[derive(Debug, Deserialize)]
-    pub struct Response {
-        pub data: Option<Data>,
+    pub mod inclusion {
+        use super::*;
+
+        #[derive(Debug, Deserialize)]
+        pub struct Response {
+            pub data: Option<Data>,
+        }
+
+        #[derive(Debug, Deserialize)]
+        pub struct Data {
+            pub events: Option<Vec<Event>>,
+        }
+
+        #[derive(Debug, Deserialize)]
+        pub enum EventId {
+            CandidateIncluded,
+            CandidateBacked,
+            CandidateTimedOut,
+        }
+
+        #[derive(Debug, Deserialize)]
+        pub struct Event {
+            pub block_num: u32,
+            pub event_id: EventId,
+            pub params: String,
+        }
     }
 
-    #[derive(Debug, Deserialize)]
-    pub struct Data {
-        pub events: Option<Vec<Event>>,
-    }
+    pub mod disputes {
+        use super::*;
 
-    #[derive(Debug, Deserialize)]
-    pub enum EventId {
-        CandidateIncluded,
-        CandidateBacked,
-        CandidateTimedOut,
-    }
+        #[derive(Debug, Deserialize)]
+        pub struct Response {
+            pub data: Data,
+        }
 
-    #[derive(Debug, Deserialize)]
-    pub struct Event {
-        pub block_num: u32,
-        pub event_id: EventId,
-        pub params: String,
+        #[derive(Debug, Deserialize)]
+        pub struct Data {
+            pub events: Option<Vec<Event>>,
+        }
+
+        #[derive(Debug, Deserialize, PartialOrd, PartialEq, Eq, Ord)]
+        pub struct Event {
+            pub block_num: u32,
+            pub extrinsic_idx: u32,
+        }
     }
 }
 
-impl TryFrom<events::Event> for Event {
+pub mod extrinsic {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Serialize)]
+    pub struct Request {
+        pub extrinsic_index: String,
+    }
+
+    pub mod parainherent {
+        use super::{
+            super::{SessionIndex, ValidatorIndex},
+            *,
+        };
+        use std::collections::HashMap;
+
+        #[derive(Debug, Deserialize)]
+        pub struct Response {
+            pub data: Option<Data>,
+        }
+
+        #[derive(Debug, Deserialize)]
+        pub struct Data {
+            pub params: Vec<Params>,
+        }
+
+        #[derive(Debug, Deserialize)]
+        pub struct Params {
+            pub value: Value,
+        }
+
+        #[derive(Debug, Deserialize)]
+        pub struct Value {
+            pub disputes: Vec<DisputeVotes>,
+        }
+
+        #[derive(Debug, Deserialize)]
+        pub struct DisputeVotes {
+            pub session: SessionIndex,
+            pub statements: Vec<DisputeVote>,
+        }
+
+        #[derive(Debug, Deserialize)]
+        pub struct DisputeVote {
+            #[serde(rename = "col1")]
+            pub kind: HashMap<DisputeVoteKind, serde_json::Value>,
+            #[serde(rename = "col2")]
+            pub validator_index: ValidatorIndex,
+        }
+
+        #[derive(Debug, Deserialize, Eq, PartialEq, Hash)]
+        pub enum DisputeVoteKind {
+            Invalid,
+            Valid,
+        }
+    }
+}
+
+impl TryFrom<events::inclusion::Event> for InclusionEvent {
     type Error = ();
 
-    fn try_from(event: events::Event) -> Result<Self, Self::Error> {
-        use events::EventId::*;
+    fn try_from(event: events::inclusion::Event) -> Result<Self, Self::Error> {
+        use events::inclusion::EventId::*;
 
         let substr = "\"para_id\":";
         let idx = event.params.find(substr).ok_or(())? + substr.len();
@@ -68,14 +157,14 @@ impl TryFrom<events::Event> for Event {
     }
 }
 
-pub async fn fetch(
+pub async fn fetch_inclusion_events(
     network: &str,
     up_to_block: u32,
     para_id: u32,
     enough_events: usize,
-) -> anyhow::Result<Vec<Event>> {
+) -> anyhow::Result<Vec<InclusionEvent>> {
     let url = format!("https://{network}.api.subscan.io/api/scan/events");
-    let mut events: Vec<Event> = Vec::new();
+    let mut events: Vec<InclusionEvent> = Vec::new();
     eprintln!("Fetching {enough_events} events for {network}, para_id({para_id}) up to block {up_to_block}");
     let pb = ProgressBar::new(enough_events as u64);
     let mut block_num = up_to_block;
@@ -85,17 +174,19 @@ pub async fn fetch(
             page: 0,
             module: "parainclusion",
             block_num: Some(block_num),
+            call: None,
+            block_range: None,
         };
         let client = reqwest::Client::new();
         let res = client.post(&url).json(&request).send().await?;
 
-        let response = res.json::<events::Response>().await?;
-        let new_events: Vec<Event> = response
+        let response = res.json::<events::inclusion::Response>().await?;
+        let new_events: Vec<InclusionEvent> = response
             .data
             .into_iter()
             .flat_map(|d| d.events)
             .flatten()
-            .flat_map(|e| Event::try_from(e).ok())
+            .flat_map(|e| InclusionEvent::try_from(e).ok())
             .filter(|e| e.para_id == para_id)
             .collect();
 
@@ -116,4 +207,113 @@ pub async fn fetch(
     }
 
     Ok(events)
+}
+
+pub async fn fetch_disputes_events(
+    network: &str,
+    up_to_block: u32,
+    enough_events: usize,
+) -> anyhow::Result<Vec<events::disputes::Event>> {
+    let url = format!("https://{network}.api.subscan.io/api/scan/events");
+    let mut disputes_initiated: Vec<events::disputes::Event> = Vec::new();
+    let pb = ProgressBar::new(enough_events as u64);
+    let from_block = up_to_block.saturating_sub(400_000); // HACK
+    let range = format!("{from_block}-{up_to_block}");
+    let mut page = 0;
+    while disputes_initiated.len() < enough_events {
+        let request = events::Request {
+            row: 100,
+            page,
+            module: "parasdisputes",
+            call: Some("disputeinitiated"),
+            block_range: Some(range.clone()),
+            block_num: None,
+        };
+        let client = reqwest::Client::new();
+        let res = client.post(&url).json(&request).send().await?;
+
+        let response = res.json::<events::disputes::Response>().await?;
+        let new_events: Vec<events::disputes::Event> =
+            response.data.events.into_iter().flatten().collect();
+
+        pb.inc(new_events.len() as u64);
+        page += 1;
+
+        if new_events.is_empty() {
+            break;
+        }
+        disputes_initiated.extend(new_events);
+        // don't trigger rate limiting
+        sleep(Duration::from_millis(150)).await;
+    }
+    pb.finish_with_message("Fetching complete!");
+
+    disputes_initiated.sort();
+    disputes_initiated.dedup();
+
+    eprintln!(
+        "Fetched {} DisputeInitiated unique events",
+        disputes_initiated.len()
+    );
+
+    Ok(disputes_initiated)
+}
+
+#[derive(serde::Serialize)]
+pub struct DisputeInitiated {
+    pub session_index: SessionIndex,
+    pub block_num: u32,
+    pub validator_index: ValidatorIndex,
+}
+
+pub async fn fetch_dispute_initiators(
+    network: &str,
+    events: Vec<events::disputes::Event>,
+) -> anyhow::Result<Vec<DisputeInitiated>> {
+    let url = format!("https://{network}.api.subscan.io/api/scan/extrinsic");
+    let mut initiators = Vec::new();
+    for event in events {
+        let events::disputes::Event {
+            block_num,
+            extrinsic_idx,
+        } = event;
+
+        let request = extrinsic::Request {
+            extrinsic_index: format!("{block_num}-{extrinsic_idx}"),
+        };
+
+        let client = reqwest::Client::new();
+        let res = client.post(&url).json(&request).send().await?;
+
+        let response = res
+            .json::<extrinsic::parainherent::Response>()
+            .await
+            .with_context(|| {
+                format!("unexpected response for parainherent {block_num}-{extrinsic_idx}")
+            })?;
+        let mut data = match response.data {
+            Some(data) => data,
+            None => {
+                eprintln!("null response for extrinsic {block_num}-{extrinsic_idx}, skipping",);
+                continue;
+            }
+        };
+        let disputes: Vec<extrinsic::parainherent::DisputeVotes> =
+            data.params.remove(0).value.disputes;
+
+        for votes in disputes {
+            let session_index = votes.session;
+            for vote in votes.statements {
+                let invalid = extrinsic::parainherent::DisputeVoteKind::Invalid;
+                if vote.kind.contains_key(&invalid) {
+                    initiators.push(DisputeInitiated {
+                        session_index,
+                        block_num,
+                        validator_index: vote.validator_index,
+                    });
+                }
+            }
+        }
+    }
+    Ok(initiators)
 }
