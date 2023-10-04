132d1dd521d1c6a15e939a7e18ddb08957278a02
Marcin S <marcin@realemail.net>
2023-10-04 18:39:34 +0200

    Quarterly oil change (#3)
    Updates to use the latest code in the Polkadot SDK repo.
    
    I also updated dependencies, because I was getting a build error with `rustc
    1.74.0-nightly (8550f15e1 2023-08-27)`.

--- src/primitives.rs
+++ src/primitives.rs
@@ -4,6 +4,6 @@ pub use crate::subxt::polkadot::runtime_types::polkadot_parachain::primitives::{
 pub use crate::subxt::polkadot::runtime_types::polkadot_primitives::v2::CandidateReceipt;
 pub use ::subxt::utils::{H256, AccountId32};
 pub use polkadot_node_primitives::AvailableData;
-pub use polkadot_parachain::primitives::{BlockData, ValidationParams};
+pub use polkadot_parachain_primitives::primitives::{BlockData, ValidationParams};
 pub type SessionIndex = u32;
 pub type ValidatorIndex = u32;
