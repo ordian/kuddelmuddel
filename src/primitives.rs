c0f328bcb1760aea92e0c1e580e96cb88d53e2c2
Marcin S <marcin@realemail.net>
2023-11-21 14:41:21 +0100

    Update: November 2023 Edition (#4)

--- src/primitives.rs
+++ src/primitives.rs
@@ -2,7 +2,7 @@ pub use crate::subxt::polkadot::runtime_types::polkadot_parachain::primitives::{
     ValidationCode, ValidationCodeHash,
 };
 pub use crate::subxt::polkadot::runtime_types::polkadot_primitives::v2::CandidateReceipt;
-pub use ::subxt::utils::{H256, AccountId32};
+pub use ::subxt::utils::{AccountId32, H256};
 pub use polkadot_node_primitives::AvailableData;
 pub use polkadot_parachain_primitives::primitives::{BlockData, ValidationParams};
 pub type SessionIndex = u32;
